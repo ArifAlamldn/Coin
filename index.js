9f6150999fe739209a8a8b6dc803dab1eb2a86a8
arif alam <arifalamldn@gmail.com>
2025-03-30 18:38:09 +0530

    code

---
+++ index.js
@@ -0,0 +1,19 @@
+document.getElementById("chat-btn").addEventListener("click", function() {
+    animateText("Path");
+});
+
+document.getElementById("path-btn").addEventListener("click", function() {
+    animateText("Chat");
+});
+
+function animateText(newText) {
+    let text = document.getElementById("animated-text");
+    text.style.opacity = "0";
+    text.style.transform = "scale(1.5) rotate(180deg)";
+    
+    setTimeout(() => {
+        text.innerText = newText;
+        text.style.opacity = "1";
+        text.style.transform = "scale(1) rotate(0deg)";
+    }, 500);
+}
