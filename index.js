04bb90d95f6abf5187506e91955663c91183eb6f
arif alam <arifalamldn@gmail.com>
2025-04-03 21:23:49 +0530

    code

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ document.getElementById("chat-btn").addEventListener("click", function() {
 
 
 
+
 document.getElementById("path-btn").addEventListener("click", function() {
     animateText("Chat");
 });
