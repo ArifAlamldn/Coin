5bd7e3fe6c3efb14c009f0ba84e4d1bb75f62fa7
arif alam <arifalamldn@gmail.com>
2025-04-05 19:41:53 +0530

    cde

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ document.getElementById("chat-btn").addEventListener("click", function() {
 
 
 
+
 document.getElementById("path-btn").addEventListener("click", function() {
     animateText("Chat");
 });
